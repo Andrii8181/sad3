1d32f4810c2c2aba9ea57f431c39bbc24cce1f43
Andrii8181 <andrii_ch@ukr.net>
2025-08-20 16:22:03 +0300

    Update about.py

--- about.py
+++ about.py
@@ -1,22 +1,8 @@
-from PyQt5.QtWidgets import QDialog, QVBoxLayout, QLabel, QPushButton
+from PyQt5.QtWidgets import QMessageBox
 
-class AboutDialog(QDialog):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("Про програму")
-        self.setGeometry(300, 200, 400, 200)
-
-        layout = QVBoxLayout()
-        text = QLabel(
-            "SAD – Статистичний аналіз даних\n\n"
-            "Розробник: Чаплоуцький А.М.\n"
-            "Кафедра плодівництва і виноградарства УНУ\n"
-            "Всі права захищені. Програма поширюється під академічною ліцензією.\n"
-        )
-        layout.addWidget(text)
-
-        close_btn = QPushButton("Закрити")
-        close_btn.clicked.connect(self.close)
-        layout.addWidget(close_btn)
-
-        self.setLayout(layout)
+def show_about():
+    QMessageBox.information(None, "Про програму", 
+        "SAD - Статистичний аналіз даних\n\n"
+        "Розробник: Чаплоуцький А.М.\n"
+        "Кафедра плодівництва і виноградарства УНУ\n\n"
+        "© Усі права захищені. Ліцензія лише для наукового використання.")
